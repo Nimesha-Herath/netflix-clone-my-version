3dab72f1287f73b9ff8820653161f42269af6f23
lunariweb <lunaristudiosglobal@gmail.com>
2021-02-12 14:37:27 +0530

    user auth set up done with firebase, next up redux

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyB4y4bkqabAfHZaLMHe59ew65EnvArEd4Q",
@@ -8,3 +10,11 @@ const firebaseConfig = {
   appId: "1:957823805161:web:8d48f013f44e61d4313278",
   measurementId: "G-D6DSWBH514",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
